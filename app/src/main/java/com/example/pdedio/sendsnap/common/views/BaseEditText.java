cc557471321762c694f0fad9d694717f97c2135e
Pawel Dedio <paweldedio@gmail.com>
2017-02-23 18:50:20 +0100

    View for log in and sign up forms

--- app/src/main/java/com/example/pdedio/sendsnap/common/views/BaseEditText.java
+++ app/src/main/java/com/example/pdedio/sendsnap/common/views/BaseEditText.java
@@ -1,8 +1,8 @@
 package com.example.pdedio.sendsnap.common.views;
 
 import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
-import android.widget.EditText;
 
 import org.androidannotations.annotations.EView;
 
@@ -10,7 +10,7 @@ import org.androidannotations.annotations.EView;
  * Created by p.dedio on 03.11.16.
  */
 @EView
-public class BaseEditText extends EditText {
+public class BaseEditText extends AppCompatEditText {
 
     public BaseEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
