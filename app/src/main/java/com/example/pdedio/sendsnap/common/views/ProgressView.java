68b0ddb552153b62199478a11ada9c78f8bbe7e6
Pawel Dedio <paweldedio@gmail.com>
2017-03-22 14:49:43 +0100

    Changed progress view

--- app/src/main/java/com/example/pdedio/sendsnap/common/views/ProgressView.java
+++ app/src/main/java/com/example/pdedio/sendsnap/common/views/ProgressView.java
@@ -5,21 +5,14 @@ import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
 import com.example.pdedio.sendsnap.R;
-import com.github.glomadrian.roadrunner.IndeterminateRoadRunner;
 
 import org.androidannotations.annotations.EViewGroup;
-import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by pawel on 11.03.2017.
  */
 @EViewGroup(R.layout.view_progress)
 public class ProgressView extends RelativeLayout {
-
-
-    @ViewById(R.id.vProgressView)
-    protected IndeterminateRoadRunner progressView;
-
 
 
     public ProgressView(Context context) {
@@ -33,4 +26,5 @@ public class ProgressView extends RelativeLayout {
     public ProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
 }
